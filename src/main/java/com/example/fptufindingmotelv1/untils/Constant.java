d0939317d65b88222c3e3a0d22cd8295787fc8b1
truongnqse05461 <truong10011998@gmail.com>
2020-09-06 16:07:30 +0700

    commit

--- src/main/java/com/example/fptufindingmotelv1/untils/Constant.java
+++ src/main/java/com/example/fptufindingmotelv1/untils/Constant.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Page;
 public class Constant {
     public static final int BUTTONS_TO_SHOW = 9;
     public static final int INITIAL_PAGE = 0;
-    public static final int INITIAL_PAGE_SIZE = 9;
+    public static final int INITIAL_PAGE_SIZE = 6;
     //public static final int[] PAGE_SIZES = {3, 6};
     public static final String DATE_FORMAT = "HH:mm:ss dd/MM/yyyy";
     public static final int NUMBER_OF_BAN_DATE_POST = 3;
